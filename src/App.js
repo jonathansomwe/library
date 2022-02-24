2570549feb3f58871337223c2925fa1b11f2e173
jonathan <lukongosomwe@gmail.com>
2022-02-24 19:30:03 +0300

    <update>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button,UncontrolledCarousel,Pagination, PaginationItem,PaginationLink} from 'reactstrap';
+import { Button,Container,Table,Row,Col,UncontrolledCarousel,Pagination, PaginationItem,PaginationLink} from 'reactstrap';
 function App() {
   return (
     <div>
-		<script src="https://fontawesome.com/a076d05399.js"></script>
-  <nav>
+  <Row>
+    <Col className="background">
+    <nav>
 	  <input type="checkbox" id="check"></input>
 		  <label for="check" className="checkbtn">
 			  <i className="fas fa-bars"></i>
@@ -19,6 +20,77 @@ function App() {
 			<li><a href="#">Contact</a></li>
 		</ul>
 	</nav>
+    </Col>
+  </Row>
+		<script src="https://fontawesome.com/a076d05399.js"></script>
+  <Container>
+    <Row>
+      <Col className='.col-6' xs='6'>
+      <Table bordered>
+  <thead>
+    <tr>
+      <th>
+        #
+      </th>
+      <th>
+        First Name
+      </th>
+      <th>
+        Last Name
+      </th>
+      <th>
+        Username
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">
+        1
+      </th>
+      <td>
+        Mark
+      </td>
+      <td>
+        Otto
+      </td>
+      <td>
+        @mdo
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">
+        2
+      </th>
+      <td>
+        Jacob
+      </td>
+      <td>
+        Thornton
+      </td>
+      <td>
+        @fat
+      </td>
+    </tr>
+    <tr>
+      <th scope="row">
+        3
+      </th>
+      <td>
+        Larry
+      </td>
+      <td>
+        the Bird
+      </td>
+      <td>
+        @twitter
+      </td>
+    </tr>
+  </tbody>
+</Table>
+      </Col>
+    </Row>
+  </Container>
 	<Button color="danger">Danger!</Button>
 	<UncontrolledCarousel
   items={[
